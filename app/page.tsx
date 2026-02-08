9df7f4b006e29e5269590345acb4c80e075346cd
Quentin Sébire <dev@quentin-sebire.fr>
2026-02-08 16:38:22 +0100

    Add suspense for useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { EndGameMessage } from "@/components/EndGameMessage";
 import { Instructions } from "@/components/Instructions";
 import { StartGame } from "@/components/StartGame";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <div className="space-y-12">
-        <EndGameMessage />
+        <Suspense fallback={<div>Loading...</div>}>
+          <EndGameMessage />
+        </Suspense>
         <div className="text-center">
           <div className="space-y-8">
             <h2 className="font-fascinate text-4xl sm:text-5xl md:text-6xl">
